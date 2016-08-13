0ac64c1d3a3eee822782b6a31e0b1142000e2fb8
drewkosta <drewkosta@gmail.com>
2016-08-12 20:14:32 -0700

    Minor Polish and Start of Youtube Implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
 class App extends React.Component {
   constructor () {
     super();
+    this.getVideo();
     this.state = {
       // Initialize the state of App to keep track of all the videos in the video list and the current video in the player. Pass this state down as props to its children components. Continue to use the example data.
       currentVideo: window.fakeVideoData[0],
       videoList: window.fakeVideoData
     };
   }
+
+  getVideo () {
+    $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/channels',
+      type: 'GET',
+      data: {
+        q: 'cats',
+        part: 'snippet',
+        key: window.YOUTUBE_API_KEY
+      },
+      success: data => {
+        console.log(data);
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+
+  }
+
   onClickHandler (video) {
     this.setState({
       currentVideo: video
